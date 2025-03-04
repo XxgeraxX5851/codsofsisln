8e5f500c588891c4cbc4d78e017adb05eaa72f79
XxgeraxX5851 <gutierrez.gerardo.2024@cbtis-246.edu.mx>
2025-03-04 15:55:39 -0600

    Update practica4.py

--- practica4.py
+++ practica4.py
@@ -1,1 +1,8 @@
+#espacios en blanco
+nombre = input("Cual es tu nombre")
+edad = input("Que edad tienes")
+profesion = input("Que deseas estudiar")
 
+print("mi nombre es",nombre)
+print("mi edad es",edad)
+print("deseo estudiar",profesion) 
